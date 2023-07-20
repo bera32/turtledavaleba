89129beaea0cced85bcb0c3ad13f61713ead7132
bera32 <139687400+bera32@users.noreply.github.com>
2023-07-20 16:42:56 +0200

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,105 @@
+from turtle import*
+shape("turtle")
+
+width (7)
+color ("purple")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+
+forward(200)
+left(90)
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+
+forward(100)
+right(90)
+
+forward (60)
+right(90)
+
+forward(100)
+end_fill()
+
+penup()
+goto(130,45)
+pendown()
+color("brown")
+forward(10)
+
+
+penup()
+goto(200,200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+penup()
+goto(180,180)
+pendown()
+color("brown")
+begin_fill()
+right(60)
+forward(40)
+left (90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+end_fill()
+
+penup()
+goto (20,140)
+pendown()
+begin_fill()
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+end_fill()
+
+penup()
+goto(210,10)
+pendown()
+
+color ("green")
+begin_fill()
+right(90)
+forward(50)
+right(90)
+forward(90)
+right(90)
+forward(200)
+right(90)
+forward(400)
+right(90)
+forward(200)
+right (90)
+forward (90)
+right(90)
+forward (50)
+end_fill()
+
+
+
+
+exitonclick()
